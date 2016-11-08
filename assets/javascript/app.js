7671e0aeeea99e5ce71b40b9b1cee8264e30b9a6
PocketThi3f <krslai@yahoo.com>
2016-11-08 12:36:59 -0800

    goingdowntown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,16 +38,38 @@
     var newTrain = childSnapshot.val().trainName
     var newLocation = childSnapshot.val().destination
     var newFirstTrain = childSnapshot.val().firstTrain
-    var newFreq = childSnapshot.val().frequency
+    var newFreq = childSnapshot.val().frequency 
 
-    var currentTime = moment().format('hh:mm');
+    var firstTimeConverted = moment(newFirstTrain,"hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var thePresent = moment();
+    console.log("It is presently: " + moment(thePresent).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("Difference in time comes out: " + diffTime);
+
+    // Time apart (remainder)
+    var leftOvers = diffTime % newFreq;
+    console.log(leftOvers);
+
+    // Minute Until Train
+    var minutesTillNext = newFreq - leftOvers;
+    console.log("MINUTES TILL TRAIN: " + minutesTillNext);
+
+    // Next Train
+    var nextTrain = moment().add(minutesTillNext, "minutes");
+    var catchTrain = moment(nextTrain).format("hh:mm");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     // List of static items to appear
     $('#trainSchedule').append("<tr><td>"
       +newTrain+" </td><td> "
       +newLocation+" </td><td> "
-      +newFreq+" </td><td> "+
-      +" </td></tr>");
+      +newFreq+" </td><td> "+catchTrain
+      +" </td><td> "+minutesTillNext+" </tr>");
 
 
     // Handle the errors
